26bcbe5d1d42afbced84160a164e01d501f0f015
Calin Sanduta <2258302@cmontmorency.qc.ca>
2024-05-06 15:05:46 -0400

    CS Création data pour oeuvres et artistes

---
+++ src/data/artistes.js
@@ -0,0 +1,24 @@
+export const ARTISTES = [
+    {
+      id: "a1",
+      nom: "Leonard de Vinci",
+      motDePasse: "test",
+      role: "normal",
+      oeuvres: ["o1","o2"],
+    },
+    {
+      id: "a2",
+      nom: "Vincent van Gogh",
+      motDePasse: "admin",
+      role: "admin",
+      oeuvres: ["o2", "o3"],
+    },
+    {
+      id: "a3",
+      nom: "Claude Monet",
+      motDePasse: "orlando",
+      role: "normal",
+      oeuvres: ["o2"],
+    },
+  ];
+  
---
+++ src/data/oeuvres.js
@@ -0,0 +1,24 @@
+export const OEUVRES = [
+    {
+      id: "o1",
+      titre: "La Joconde",
+      description: "Célèbre peinture de Leonard de Vinci.",
+      artiste: "Leonard de Vinci",
+      image: require("../assets/LaJoconde.jpg"),
+    },
+    {
+      id: "o2",
+      titre: "Les Tournesols",
+      description: "Série de peintures de Vincent van Gogh.",
+      artiste: "Vincent van Gogh",
+      image: require("../assets/LesTournesols.jpg"),
+    },
+    {
+      id: "o3",
+      titre: "La Nuit étoilée",
+      description: "Peinture emblématique de Vincent van Gogh.",
+      artiste: "Vincent van Gogh",
+      image: require("../assets/LaNuitétoilée.jpg"),
+    },
+  ];
+  
