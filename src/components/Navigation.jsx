7dafff2cfe8e77e7e13df7d174c933753d47f360
Mamoune Bastor <mamounebastor7@gmail.com>
2024-05-07 17:10:12 -0400

    Ajout de navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,27 +2,29 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/connexion">Connexion</Link>
-        </li>
-        <li>
-          <Link to="/inscription">Inscription</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/parametres">Paramètres</Link>
-        </li>
-        <li>
-          <Link to="/artistes">Liste d'artistes</Link>
-        </li>
-        <li>
-          <Link to="/oeuvre">Liste des oeuvres d'art</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="container">
+        <ul>
+          <li>
+            <Link to="/connexion">Connexion</Link>
+          </li>
+          <li>
+            <Link to="/inscription">Inscription</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/parametres">Paramètres</Link>
+          </li>
+          <li>
+            <Link to="/artistes">Liste d'artistes</Link>
+          </li>
+          <li>
+            <Link to="/oeuvre">Liste des oeuvres d'art</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
