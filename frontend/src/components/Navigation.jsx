e5ee6fc8cfe1257843e4f87f9f04743384c3f650
Calin Sanduta <2258302@cmontmorency.qc.ca>
2024-05-11 22:22:26 -0400

    CS ajustement avec isLoggedIn

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthentificationContexte } from "./contexte/AuthentificationContext";
 
 export default function Navigation() {
+  const auth = useContext(AuthentificationContexte);
   return (
     <nav className="navbar">
       <div className="container">
@@ -11,12 +14,16 @@ export default function Navigation() {
           <li>
             <Link to="/inscription">Inscription</Link>
           </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/parametres">Paramètres</Link>
-          </li>
+          {auth.isLoggedIn && ( 
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/parametres">Paramètres</Link>
+              </li>
+            </>
+          )}
           <li>
             <Link to="/artistes">Liste d'artistes</Link>
           </li>
